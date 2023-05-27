6a59944142d1f6bc5284fbbc6574fdcf65fc0c6e
ALEKSANDR <pavlenkoaleksandr270397@gmail.com>
2023-05-27 09:57:25 +0500

    14

---
+++ homework2/task14.py
@@ -0,0 +1,15 @@
+#Задача 14: 
+N = int(input('Введите число N '))
+print(f"Степени двойки, не больше {N}:")
+stepen2=1
+print(stepen2)
+while stepen2<=N :
+    stepen2*=2
+    if stepen2<=N:
+         print(stepen2)
+
+
+
+
+
+      
